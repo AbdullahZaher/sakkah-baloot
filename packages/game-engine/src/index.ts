03f59e29de2d9cf5d8ef3ddaeee0c526c4c060b5
Abdullah Zaher <mansummits@gmail.com>
2026-09-22 21:50:35 +0300

    feat(engine): export bidding and escalation

--- packages/game-engine/src/index.ts
+++ packages/game-engine/src/index.ts
@@ -2,3 +2,5 @@ export * from "./rules/types.js";
 export * from "./rules/profile.js";
 export * from "./cards.js";
 export * from "./dealing/deal-engine.js";
+export * from "./bidding/bidding-engine.js";
+export * from "./escalation.js";
