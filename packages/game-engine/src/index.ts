04a9d6bd5e227c69822e7c7b245182ff63739bff
Abdullah Zaher <mansummits@gmail.com>
2026-09-22 21:55:34 +0300

    feat(engine): export project and legal move modules

--- packages/game-engine/src/index.ts
+++ packages/game-engine/src/index.ts
@@ -4,3 +4,7 @@ export * from "./cards.js";
 export * from "./dealing/deal-engine.js";
 export * from "./bidding/bidding-engine.js";
 export * from "./escalation.js";
+
+export * from "./projects/project-engine.js";
+export * from "./playing/types.js";
+export * from "./playing/legal-moves.js";
